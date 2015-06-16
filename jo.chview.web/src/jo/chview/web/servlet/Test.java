5a8cf84faa10efd283107fd2b884a2baef4e319c
Jo Jaquinta <jo@111george.com>
2015-06-15 21:32:50 -0400

    Pre-beta check-in

---
+++ jo.chview.web/src/jo/chview/web/servlet/Test.java
@@ -0,0 +1,33 @@
+package jo.chview.web.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Test
+ */
+@WebServlet("/Test")
+public class Test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Test() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/plain");
+		response.getOutputStream().write("Now is the time for all good men to come to the aid of the nation.".getBytes());
+		System.getProperties().store(response.getOutputStream(), "System Properties");
+	}
+
+}
